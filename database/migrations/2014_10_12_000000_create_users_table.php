cb69f81565b2c52097a833d07024c145cfc465eb
sharminex81 <mimetyshanta@gmail.com>
2017-10-28 16:59:10 +0600

    Changes something to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,16 +14,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('uuid');
-            $table->string('username');
-            $table->string('email_address')->unique();
-            $table->string('password');
-            $table->string('password_token');
-            $table->integer('status');
-            $table->integer('is_vissible');
+            $table->string('uuid', 36);
+            $table->string('username', 120);
+            $table->string('email_address', 120)->unique();
+            $table->string('password', 36);
+            $table->string('password_token', 120);
+            $table->tinyInteger('status')->default(1)->comment('1 = Active, 2 = Inactive');
+            $table->tinyInteger('is_vissible')->default(1)->comment('1 = Visible, 2 = Disable');
             $table->dateTime('last_seen');
             $table->rememberToken();
             $table->timestamps();
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
         });
     }
 
