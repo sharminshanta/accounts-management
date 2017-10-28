52cf8674ac887c8bf65f7af5ab32e6f558286509
sharminex81 <mimetyshanta@gmail.com>
2017-10-28 16:58:41 +0600

    Added users role and roles table

---
+++ database/migrations/2017_10_28_094541_create_roles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uuid', 36);
+            $table->string('name', 80);
+            $table->string('slug', 45);
+            $table->string('description', 120);
+            $table->tinyInteger('is_locked')->default(1)->comment('1 = Locked, 2 = Unlocked');
+            $table->timestamps();
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
---
+++ database/migrations/2017_10_28_104304_create_users_roles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('role_id');
+            $table->tinyInteger('is_active')->default(1)->comment('1 = Active, 2 = Inactive');
+            $table->timestamps();
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_roles');
+    }
+}
