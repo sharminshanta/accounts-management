4da88f397a0a01cc67ff7302a03d520a399c3bfa
sharminex81 <mimetyshanta@gmail.com>
2017-10-29 10:45:49 +0600

    something changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,8 @@
 @section('title', 'Dashboard')
 @section('content')
     <div class="row">
-        {{ Session::get('authinfo') }}
+        {{ $user = \App\User::details(Session::get('authinfo'))  }}
+        {{ $user['username'] }}
         <div class="col-lg-3 col-sm-6">
             <!-- START widget-->
             <div class="panel bg-info-light pt b0 widget">
