9d426fc381821f23372177091b3b29ad8e58ec28
sharminex81 <mimetyshanta@gmail.com>
2017-11-01 14:12:25 +0600

    Users)addressed table is done

---
+++ database/migrations/2017_11_01_075846_create_users_addresses_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('users_addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uuid', 36)->unique();
+            $table->integer('user_id');
+            $table->string('street', 80)->default(null);
+            $table->string('street_secondary', 80)->default(null);
+            $table->string('city', 80)->default(null);
+            $table->string('state', 80)->default(null);
+            $table->string('postal_code', 80)->default(null);
+            $table->string('country', 80)->default(null);
+            $table->string('phone', 80)->default(null);
+            $table->string('fax', 80)->default(null);
+            $table->string('type')->default('Primary');
+            $table->timestamps();
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_addresses');
+    }
+}
